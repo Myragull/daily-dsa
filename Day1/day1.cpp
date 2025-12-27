997d6f4a3514490de3081885e6fb2eacff0959a8
Myra Gull <myragull749@gmail.com>
2025-12-28 04:11:31 +0500

    Created a new repo for the dialy DSA qs

---
+++ Day1/day1.cpp
@@ -0,0 +1,21 @@
+                //  FlowCharts and pseudoCode:
+
+// Qno1: Find whether a number is even or odd
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (number % 2 == 0)
+    {
+        cout << number << " is even." << endl;
+    }
+    else
+    {
+        cout << number << " is odd." << endl;
+    }
+    return 0;
+}
+
