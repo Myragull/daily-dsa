d9cb3fa895d4ae3ba8da1fbb8349313faaf6d8ed
Myra Gull <myragull749@gmail.com>
2025-12-28 04:19:59 +0500

    Added qno 2,3 and datatypes

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -19,3 +19,48 @@ int main()
     return 0;
 }
 
+// Qno2: Print nums form 1 to 5
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
+
+// Qno3: Print sum fo all evens from 1 to n
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+    }
+    cout << "Sum of all even numbers from 1 to " << n << " is " << sum << endl;
+    return 0;
+}
+
+                       //  DATATYPES in C++:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    cout << n << endl;
+    char ch = 'q';
+    cout << ch << endl;
+    float f = 10.5;
+    cout << f << endl;
+    return 0;
+}
