010ea41d2211e82ff7cfb6ebde06065cdca98960
Myra Gull <myragull749@gmail.com>
2025-12-28 10:53:50 +0500

    Added 4 papttern qs

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -1,23 +1,134 @@
 // Qno2: Write a program to check whether a character is an uppercase letter, a lowercase letter, or a numeric digit.
 
-#include<iostream>
-using namespace std;
-int main(){
-char ch;
-cout<<"Enter a character : ";
-cin>>ch;
+// #include<iostream>
+// using namespace std;
+// int main(){
+// char ch;
+// cout<<"Enter a character : ";
+// cin>>ch;
 
-if(ch>='a' && ch<='z'){
-    cout<<"Lower case";
-}else if(ch>='A' && ch<='Z'){
-    cout<<"Upper case";
-}else if(ch>='0' && ch<='9'){
-    cout<<"Numeric";
-}
+// if(ch>='a' && ch<='z'){
+//     cout<<"Lower case";
+// }else if(ch>='A' && ch<='Z'){
+//     cout<<"Upper case";
+// }else if(ch>='0' && ch<='9'){
+//     cout<<"Numeric";
+// }
 
-return 0;
+// return 0;
 
-}
+// }
 
                                 //    Pattern Questions
-                                
+// 1 print following pattern of loops
+
+// ****
+// ****
+// ****
+// ****     
+
+// #include<iostream>
+// using namespace std;
+
+// int main (){
+
+// int n;
+// cout<<"Enter number : ";
+// cin>>n;
+// int i=1;
+// while(i<=n){
+// int j=1;
+// while(j<=n){
+//     cout<<"*";
+//     j++;
+// }
+// cout<<endl;
+// i++;
+// }
+// }
+
+
+//2 : Print the following patterns:
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
+// #include<iostream>
+// using namespace std;
+
+// int main (){
+
+// int n;
+// cout<<"Enter number : ";
+// cin>>n;
+// int i=1;
+// while(i<=n){
+// int j=1;
+// while(j<=n){
+//     cout<<j;
+//     j++;
+// }
+// cout<<endl;
+// i++;
+// }
+// }
+
+// 3
+    // 54321
+    // 54321
+    // 54321
+    // 54321
+    // 54321
+    // 54321
+
+// #include<iostream>
+// using namespace std;
+
+// int main (){
+
+// int n;
+// cout<<"Enter number : ";
+// cin>>n;
+// int i=1;
+// while(i<=n){
+// int j=1;
+// while(j<=n){
+//     cout<<n-j+1;
+//     j++;
+// }
+// cout<<endl;
+// i++;
+// }
+// }
+
+
+
+//4
+    // 1 2 3 
+    // 4 5 6 
+    // 7 8 9
+#include<iostream>
+using namespace std;
+
+int main (){
+
+int n;
+cout<<"Enter number : ";
+cin>>n;
+int i=1;
+int count=1;
+while(i<=n){
+int j=1;
+while(j<=n){
+    cout<<count<<" ";
+    count++;
+    j++;
+}
+cout<<endl;
+i++;
+}
+}
+
+
+//5
