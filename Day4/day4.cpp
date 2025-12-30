7c62949cb988ae03fc486629433e07c124ec6990
Myra Gull <myragull749@gmail.com>
2025-12-30 06:46:06 +0500

    Added a qs for the conversion of numbers from decimal to binary

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -133,3 +133,28 @@ for (int i = 0; i < 5; i++)
 }
 return 0;
 }
+
+// convert decimal to binary numbers
+// using bits
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+int n;
+cout<<"Enter a number : ";
+cin>>n;
+
+int i=0;
+int ans=0;
+while(n!=0)
+{
+int bit=n&1;
+ans=(bit*pow(10,i))+ans;
+i++;
+n=n>>1;
+}
+cout<<ans;
+return 0;
+}
