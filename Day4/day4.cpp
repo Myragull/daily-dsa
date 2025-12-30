74fad30f0211aad48ff487c8439db98ec84da2af
Myra Gull <myragull749@gmail.com>
2025-12-30 06:41:42 +0500

    Added qs of continue statements

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -23,3 +23,113 @@ int main()
     return 0;
 }
 
+// continue statements
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 3)
+        {
+            continue;
+        }
+        cout << i << " ";
+    }
+    return 0;
+}
+
+// practice qs what will be the output of the following questions:
+//    1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+cout << "Enter a number : ";
+cin >> n;
+
+for (int i = 0; i <= n; i++)
+{
+    cout << i << " ";
+    i++;
+}
+return 0;
+}
+
+// 2
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cout << "Enter a number : ";
+cin >> n;
+
+for (int i = 0; i <= n; i--)
+{
+    cout << i << " ";
+    i++;
+}
+return 0;
+}
+
+// 3
+int n;
+cout<<"Enter a number :";
+cin>>n;
+int n;
+cout << "Enter a number : ";
+cin >> n;
+
+for (int i = 0; i <= 15; i = i + 2)
+{
+    cout << i << " ";
+    if (i & 1)
+    {
+        continue;
+    }
+    i++;
+}
+return 0;
+}
+
+// 4
+int n;
+cout<<"Enter a number :";
+cin>>n;
+for (int i = 0; i < 5; i++)
+{
+    for (int j = i; j < 5; j++)
+    {
+        cout << i << " " << j << endl;
+    }
+}
+return 0;
+}
+
+// 5
+#include<iostream>
+using namespace std;
+
+int main(){
+for (int i = 0; i < 5; i++)
+{
+    for (int j = i; j <= 5; j++)
+    {
+        if (i + j == 10)
+        {
+            break;
+        }
+        cout << i << " " << j << endl;
+    }
+}
+return 0;
+}
