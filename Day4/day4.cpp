6def1e7387434e8051086d03c8de5a9f18f55593
Myra Gull <myragull749@gmail.com>
2025-12-31 07:18:28 +0500

    Added 1st qs of conversion of binary to decimal of day 5

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -1,51 +1,51 @@
 // 1 fibonaci series
 //  0 1 1 2 3 5 8 13 21 34
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    int n;
-    cout << "Enter a number :";
-    cin >> n;
-    int a = 0;
-    int b = 1;
-    cout << a << " " << b << " ";
+// int main()
+// {
+//     int n;
+//     cout << "Enter a number :";
+//     cin >> n;
+//     int a = 0;
+//     int b = 1;
+//     cout << a << " " << b << " ";
 
-    for (int i = 1; i <= n; i++)
-    {
-        int nextNum = a + b;
-        cout << nextNum << " ";
-        a = b;
-        b = nextNum;
-    }
-    return 0;
-}
+//     for (int i = 1; i <= n; i++)
+//     {
+//         int nextNum = a + b;
+//         cout << nextNum << " ";
+//         a = b;
+//         b = nextNum;
+//     }
+//     return 0;
+// }
 
 // continue statements
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-    int n;
-    cout << "Enter a number : ";
-    cin >> n;
+// int main()
+// {
+//     int n;
+//     cout << "Enter a number : ";
+//     cin >> n;
 
-    for (int i = 1; i <= n; i++)
-    {
-        if (i == 3)
-        {
-            continue;
-        }
-        cout << i << " ";
-    }
-    return 0;
-}
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (i == 3)
+//         {
+//             continue;
+//         }
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
 
 // practice qs what will be the output of the following questions:
-//    1
+//  1
 
 #include<iostream>
 using namespace std;
@@ -54,7 +54,6 @@ int main(){
 int n;
 cout << "Enter a number : ";
 cin >> n;
-
 for (int i = 0; i <= n; i++)
 {
     cout << i << " ";
@@ -72,7 +71,6 @@ int main(){
 int n;
 cout << "Enter a number : ";
 cin >> n;
-
 for (int i = 0; i <= n; i--)
 {
     cout << i << " ";
@@ -81,27 +79,30 @@ for (int i = 0; i <= n; i--)
 return 0;
 }
 
-// 3
-int n;
-cout<<"Enter a number :";
-cin>>n;
-int n;
-cout << "Enter a number : ";
-cin >> n;
+// // 3
+// int n;
+// cout<<"Enter a number :";
+// cin>>n;
+// int n;
+// cout << "Enter a number : ";
+// cin >> n;
 
-for (int i = 0; i <= 15; i = i + 2)
-{
-    cout << i << " ";
-    if (i & 1)
-    {
-        continue;
-    }
-    i++;
-}
-return 0;
-}
+// for (int i = 0; i <= 15; i = i + 2)
+// {
+//     cout << i << " ";
+//     if (i & 1)
+//     {
+//         continue;
+//     }
+//     i++;
+// }
+// return 0;
+// }
 
 // 4
+#include<iostream>
+using namespace std;
+int main(){
 int n;
 cout<<"Enter a number :";
 cin>>n;
---
+++ Day5/day5.cpp
@@ -0,0 +1,23 @@
+// converts binary to decimal
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+int n;
+cout<<"Enter a number :";
+cin>>n;
+int ans=0;
+int i=0;
+while(n!=0){
+    int bit=n%10;
+    if(bit==1){
+    ans=(ans+pow(2,i));
+    }
+    i++;
+    n=n/10;
+}
+cout<<ans;
+return 0;
+}
