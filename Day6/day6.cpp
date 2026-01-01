68515e1f055fc4778192ad158a0c8909e3399c55
Myra Gull <myragull749@gmail.com>
2026-01-01 07:49:33 +0500

    Added qs of power

--- Day6/day6.cpp
+++ Day6/day6.cpp
@@ -37,3 +37,29 @@ int main()
         break;
     }
 }
+
+       // Functions
+
+// 1 power(a,b)
+#include<iostream>
+using namespace std;
+
+int power(int a,int b){
+    int ans=1;
+    for(int i=1; i<=b; i++){
+    ans=ans*a;
+    }
+    return ans;
+}
+
+int main(){
+ int a;
+cout<< "Enter value of a : ";
+cin>>a;
+int b;
+cout<<"Enter value of b : ";
+cin>>b;
+int ans1=power(a,b);
+cout<<"Answer : "<<ans1;
+return 0;
+}
