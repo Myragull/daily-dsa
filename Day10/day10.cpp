bf2b81dacf3c4d05c8fc844233d51728cd37af73
Myra Gull <myragull749@gmail.com>
2026-01-05 07:26:26 +0500

    Added bubble sort program

--- Day10/day10.cpp
+++ Day10/day10.cpp
@@ -23,3 +23,42 @@ for(int i=0; i<n; i++){
     cout<<arr[i]<<" ";
 }
 }
+
+
+
+// bubble sort
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int arr[6] = {10, 20, 2, 4, 30, 15};
+    int size = 6;
+
+    bubbleSort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
